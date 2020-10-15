2fd9129b7b6b2cc622d9164f6f2bea55029eb8f5
waningflow <jack1995woods@gmail.com>
2020-10-15 09:42:23 +0800

    feat: add cache

--- repositories.js
+++ repositories.js
@@ -1,8 +1,31 @@
 const { fetchRepositories } = require('./utils/fetch');
 
+const cache = {
+  data: {},
+  get: function (key) {
+    let result;
+    if (this.data[key]) {
+      if (this.data[key].expiredTime >= Date.now()) {
+        result = this.data[key].value;
+      } else {
+        delete this.data[key];
+      }
+    }
+    return result;
+  },
+  set: function (key, value, lifetime) {
+    this.data[key] = { value, expiredTime: Date.now() + lifetime };
+  },
+};
+
 exports.handler = async (req, resp, context) => {
   const { language, since, spoken_language_code: spokenLanguage } = req.queries;
-  const data = await fetchRepositories({ language, since, spokenLanguage });
+  const cacheKey = JSON.stringify({ language, since, spokenLanguage });
+  let data = cache.get(cacheKey);
+  if (!data) {
+    data = await fetchRepositories({ language, since, spokenLanguage });
+    cache.set(cacheKey, data, 15 * 60 * 1000);
+  }
   resp.setHeader('Content-Type', 'application/json');
   resp.setHeader('Cache-Control', 'max-age=900');
   resp.setHeader('Access-Control-Allow-Origin', '*');
