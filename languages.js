62d2d9e1e2b67ff7a61a79bf081d85824a2d36d5
waningflow <jack1995woods@gmail.com>
2020-10-15 09:28:17 +0800

    feat: update header

--- languages.js
+++ languages.js
@@ -2,5 +2,6 @@ const languages = require('./languages.json');
 
 exports.handler = (req, resp, context) => {
   resp.setHeader('Content-Type', 'application/json');
+  resp.setHeader('Access-Control-Allow-Origin', '*');
   resp.send(JSON.stringify(languages));
 };
--- repositories.js
+++ repositories.js
@@ -4,5 +4,7 @@ exports.handler = async (req, resp, context) => {
   const { language, since, spoken_language_code: spokenLanguage } = req.queries;
   const data = await fetchRepositories({ language, since, spokenLanguage });
   resp.setHeader('Content-Type', 'application/json');
+  resp.setHeader('Cache-Control', 'max-age=900');
+  resp.setHeader('Access-Control-Allow-Origin', '*');
   resp.send(JSON.stringify(data));
 };
